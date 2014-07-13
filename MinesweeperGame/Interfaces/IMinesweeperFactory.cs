e5146808921d835a86c68871b989ed3435b2ae1e
susankelvin <susankelvin@users.noreply.github.com>
2014-07-13 12:08:47 +0300

    Basic interfaces created
    IDrawer, IMinesGenerator, IScoreBoard

---
+++ MinesweeperGame/Interfaces/IMinesweeperFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MinesweeperGame.Interfaces
+{
+    public interface IMinesweeperFactory
+    {
+
+    }
+}
---
+++ MinesweeperGame/Interfaces/IScoreBoard.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MinesweeperGame.Interfaces
+{
+    interface IScoreBoard
+    {
+
+    }
+}
