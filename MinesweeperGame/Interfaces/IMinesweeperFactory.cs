9105c80a9101b5b57fe5a2eeaadcc26c81f89089
susankelvin <susankelvin@users.noreply.github.com>
2014-07-13 12:16:47 +0300

    IMinesweeperFactory defined
    Defines the "Abstract factory" design pattern factory interface.

--- MinesweeperGame/Interfaces/IMinesweeperFactory.cs
+++ MinesweeperGame/Interfaces/IMinesweeperFactory.cs
@@ -4,6 +4,22 @@ namespace MinesweeperGame.Interfaces
 {
     public interface IMinesweeperFactory
     {
+        /// <summary>
+        /// Returns UI drawer.
+        /// </summary>
+        /// <returns>IDrawer implementation.</returns>
+        IDrawer GetDrawer();
 
+        /// <summary>
+        /// Returns random mines generator.
+        /// </summary>
+        /// <returns>IMinesGenerator implementation.</returns>
+        IMinesGenerator GetMinesGenerator();
+
+        /// <summary>
+        /// Returns score board manager.
+        /// </summary>
+        /// <returns>IScoreBoard implementation.</returns>
+        IScoreBoard GetScoreBoard();
     }
 }
