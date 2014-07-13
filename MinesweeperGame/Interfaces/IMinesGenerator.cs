3707f5945f7282546212e734db9cfba1614b3dce
susankelvin <susankelvin@users.noreply.github.com>
2014-07-13 12:30:56 +0300

    IMinesGenerator defined
    Generic interface for mines generators.

--- MinesweeperGame/Interfaces/IMinesGenerator.cs
+++ MinesweeperGame/Interfaces/IMinesGenerator.cs
@@ -2,8 +2,20 @@
 
 namespace MinesweeperGame.Interfaces
 {
-    interface IMinesGenerator
+    /// <summary>
+    /// Generic interface for mines generators.
+    /// </summary>
+    public interface IMinesGenerator
     {
-
+        /// <summary>
+        /// Deploys mines at random positions.
+        /// </summary>
+        /// <param name="mines">Mines field where to deploy mines.</param>
+        /// <param name="rows">Number of rows of the field.</param>
+        /// <param name="columns">Number of columns of the field.</param>
+        /// <param name="mineCount">Number of mines to deploy.</param>
+        /// <param name="randomMines">Random number generator to use.</param>
+        /// <returns>Field of randomly positioned mines.</returns>
+        string[,] FillWithRandomMines(int rows, int columns, int mineCount, Random randomMines);
     }
 }
