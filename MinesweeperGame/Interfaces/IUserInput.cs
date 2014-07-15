b556fa3194d0fb9830874f35c5a35e1a2abe0e61
susankelvin <susankelvin@users.noreply.github.com>
2014-07-15 13:51:42 +0300

    IUserInput introduced
    Generic interface for retrieval of user input.

---
+++ MinesweeperGame/Interfaces/IUserInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinesweeperGame.Interfaces
+{
+    /// <summary>
+    /// Generic interface for retrieval of user input.
+    /// </summary>
+    public interface IUserInput
+    {
+        /// <summary>
+        /// Queries for command.
+        /// </summary>
+        /// <returns>User's command.</returns>
+        string GetCommand();
+
+        /// <summary>
+        /// Queries for user's name.
+        /// </summary>
+        /// <returns>User's name.</returns>
+        string GetUserName();
+    }
+}
