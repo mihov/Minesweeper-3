492aead8e2ccfea57227eb817a25cc0c2bb0948f
iliandj <iliandj@gmail.com>
2014-07-16 01:35:35 +0300

    Add JustMock library and first border test for minesGenerator.FillWithRandomMines

---
+++ MinesweeperGame.UnitTests/MinesGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿namespace MinesweeperGame.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MinesweeperGame;
+    using MinesweeperGame.Interfaces;
+    using System;
+    using Telerik.JustMock;
+    
+    /// <summary>
+    /// Unit test for the class MinesGenerator
+    /// </summary>
+    [TestClass]
+    public class MinesGeneratorTest
+    {
+        [TestMethod]
+        public void CheckIfSingleElementMinesFieldMatrixIsFiledWithMine()
+        {
+            IMinesweeperFactory factory = new MinesweeperFactory();
+            IMinesGenerator minesGenerator = factory.GetMinesGenerator();
+            Random random = Mock.Create<Random>();
+            string[,] minesField = minesGenerator.FillWithRandomMines(1, 1, 1, random);
+            Assert.AreNotEqual(minesField[0, 0], "");
+        }
+    }
+}
