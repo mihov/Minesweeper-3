f5cc1ce9ea2768a8ce9b57ef87e89a41de3a6606
susankelvin <susankelvin@users.noreply.github.com>
2014-07-15 12:32:07 +0300

    interface IDrawer defined
    It is the generic interface for UI display.

--- MinesweeperGame/Interfaces/IDrawer.cs
+++ MinesweeperGame/Interfaces/IDrawer.cs
@@ -2,8 +2,35 @@
 
 namespace MinesweeperGame.Interfaces
 {
+    /// <summary>
+    /// Generic interface for UI display.
+    /// </summary>
     public interface IDrawer
     {
+        /// <summary>
+        /// Displays welcome message.
+        /// </summary>
+        /// <param name="message">Welcome contents.</param>
+        void ShowWelcome(string message);
 
+        /// <summary>
+        /// Displays game end message.
+        /// </summary>
+        /// <param name="message">Game end contents.</param>
+        void ShowGameEnd(string message);
+
+        /// <summary>
+        /// Draws the mines field to the UI.
+        /// </summary>
+        /// <param name="minesField">Mines field.</param>
+        /// <param name="revealMines">If true, display the position of the mines, otherwise diplay
+        /// the contents of the already visited fields only.</param>
+        void Draw(string[,] minesField, bool revealMines = false);
+
+        /// <summary>
+        /// Display general purpose message to the user.
+        /// </summary>
+        /// <param name="message">Message to display.</param>
+        void Message(string message);
     }
 }
