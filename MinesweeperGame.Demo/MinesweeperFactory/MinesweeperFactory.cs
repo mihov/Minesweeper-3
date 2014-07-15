ba1a8eb94fe4c64ba5a7f2868d703e3da36c9a1e
susankelvin <susankelvin@users.noreply.github.com>
2014-07-15 18:03:01 +0300

    class MinesweeperFactory : IMinesweeperFactory
    The object factory of the game

---
+++ MinesweeperGame.Demo/MinesweeperFactory/MinesweeperFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MinesweeperGame.Interfaces;
+using MinesweeperGame.MineGenerator;
+using MinesweeperGame.ScoresBoard;
+
+
+namespace MinesweeperGame.Demo.MinesweeperFactory
+{
+    /// <summary>
+    /// IMinesweeperFactory implementation.
+    /// </summary>
+    class MinesweeperFactory : IMinesweeperFactory
+    {
+        public IDrawer GetDrawer()
+        {
+            return new ConsoleDrawer.ConsoleDrawer();
+        }
+
+        public IUserInput GetCommandProvider()
+        {
+            return new ConsoleInput.ConsoleInput();
+        }
+
+        public IMinesGenerator GetMinesGenerator()
+        {
+            return new MinesGenerator();
+        }
+
+        public IScoreBoard GetScoreBoard()
+        {
+            return new ScoreBoard();
+        }
+    }
+}
