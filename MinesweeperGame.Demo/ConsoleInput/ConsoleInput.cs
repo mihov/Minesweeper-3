06a071679991deb1603f817e09ebb8f327a7dc56
susankelvin <susankelvin@users.noreply.github.com>
2014-07-15 14:05:10 +0300

    class ConsoleInput : IUserInput
    Implements IUserInput for text console

---
+++ MinesweeperGame.Demo/ConsoleInput/ConsoleInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MinesweeperGame.Interfaces;
+
+namespace MinesweeperGame.Demo.ConsoleInput
+{
+    /// <summary>
+    /// Implements IUserInput for text console.
+    /// </summary>
+    public class ConsoleInput : IUserInput
+    {
+        /// <summary>
+        /// Queries for command.
+        /// </summary>
+        /// <returns>User's command.</returns>
+        public string GetCommand()
+        {
+            Console.Write("Enter row and column: ");
+            string line = Console.ReadLine();
+            return line;
+        }
+
+        /// <summary>
+        /// Queries for user's name.
+        /// </summary>
+        /// <returns>User's name.</returns>
+        public string GetUserName()
+        {
+            Console.Write("Please enter your name for the top scoreboard: ");
+            string playerName = Console.ReadLine();
+            return playerName;
+        }
+    }
+}
