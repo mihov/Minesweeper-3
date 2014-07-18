223035a9d1c1212a84f303463e33d0ddcbb65310
pepinho24 <p_mil4ev@hotmail.com>
2014-07-18 23:43:12 +0300

    Added abstract class Command in order to implement Command design pattern

---
+++ MinesweeperGame/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperGame
+{ 
+    abstract class Command  // http://www.codeproject.com/Articles/15207/Design-Patterns-Command-Pattern
+    {
+        abstract public void Redo();
+        abstract public void Undo();
+    }
+}
