a5315f198081864c165fbe1e08bf15757c4c01f3
pepinho24 <p_mil4ev@hotmail.com>
2014-07-19 17:35:25 +0300

    Added some unit tests in MinesInitializerTest.cs

--- MinesweeperGame.UnitTests/MinesInitializerTest.cs
+++ MinesweeperGame.UnitTests/MinesInitializerTest.cs
@@ -9,6 +9,7 @@ namespace MinesweeperGame.UnitTests
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using MinesweeperGame;
+    using MinesweeperGame.Interfaces;
 
     /// <summary>
     /// Used to test the <see cref="MinesInitializer"/> class.
@@ -17,9 +18,77 @@ namespace MinesweeperGame.UnitTests
     public class MinesInitializerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentNullException),
+         "Null argument is not allowed")]
+        public void TestPlayMinesMethodWithAllNullArguments()
         {
-            // TODO -> to be implemented
+            var minesInit = MinesInitializer.Instance();
+            IMinesGenerator minesGenerator = null;
+            IDrawer drawer = null;
+            IUserInput userInput = null;
+            IScoreBoard scoreBoard = null;
+            Random random = null;
+            minesInit.PlayMines(minesGenerator, drawer, userInput, scoreBoard, random);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+         "Null argument is not allowed")]
+        public void TestPlayMinesMethodWithMinesGeneratorNull()
+        {
+            var minesInit = MinesInitializer.Instance();
+            IMinesweeperFactory factory = new MinesweeperFactory();
+            IMinesGenerator minesGenerator = null;
+            IDrawer drawer = factory.GetDrawer();
+            IUserInput userInput = factory.GetCommandProvider();
+            IScoreBoard scoreBoard = factory.GetScoreBoard();
+            Random random = new Random();
+            minesInit.PlayMines(minesGenerator, drawer, userInput, scoreBoard, random);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+         "Null argument is not allowed")]
+        public void TestPlayMinesMethodWithDrawerNull()
+        {
+            var minesInit = MinesInitializer.Instance();
+            IMinesweeperFactory factory = new MinesweeperFactory();
+            IMinesGenerator minesGenerator = factory.GetMinesGenerator();
+            IDrawer drawer = null;
+            IUserInput userInput = factory.GetCommandProvider();
+            IScoreBoard scoreBoard = factory.GetScoreBoard();
+            Random random = new Random();
+            minesInit.PlayMines(minesGenerator, drawer, userInput, scoreBoard, random);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+         "Null argument is not allowed")]
+        public void TestPlayMinesMethodWithUserInputNull()
+        {
+            var minesInit = MinesInitializer.Instance();
+            IMinesweeperFactory factory = new MinesweeperFactory();
+            IMinesGenerator minesGenerator = factory.GetMinesGenerator();
+            IDrawer drawer = factory.GetDrawer();
+            IUserInput userInput = null;
+            IScoreBoard scoreBoard = factory.GetScoreBoard();
+            Random random = new Random();
+            minesInit.PlayMines(minesGenerator, drawer, userInput, scoreBoard, random);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+         "Null argument is not allowed")]
+        public void TestPlayMinesMethodWithScoreBoardNull()
+        {
+            var minesInit = MinesInitializer.Instance();
+            IMinesweeperFactory factory = new MinesweeperFactory();
+            IMinesGenerator minesGenerator = factory.GetMinesGenerator();
+            IDrawer drawer = factory.GetDrawer();
+            IUserInput userInput = factory.GetCommandProvider();
+            IScoreBoard scoreBoard = null;
+            Random random = new Random();
+            minesInit.PlayMines(minesGenerator, drawer, userInput, scoreBoard, random);
         }
     }
 }
