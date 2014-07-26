810e783cd0887c21e10a2b6cb9040edd5e3c6643
Plamen <stanev.plamen@gmail.com>
2014-07-26 13:29:39 +0300

    Adding Game fasade tests

--- MinesweeperGame.UnitTests/GameFacadeTest.cs
+++ MinesweeperGame.UnitTests/GameFacadeTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace MinesweeperGame.UnitTests
 {
@@ -7,11 +8,33 @@ namespace MinesweeperGame.UnitTests
     public class GameFacadeTest
     {
         [TestMethod]
+        public void TestInstance()
+        {
+            GameFacade minesweeperGame1 = GameFacade.Instance;
+            GameFacade minesweeperGame2 = GameFacade.Instance;
+
+            Assert.AreSame(minesweeperGame1, minesweeperGame1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+        "Line input cannot be null (empty continue command)")]
         public void TestRun()
         {
-            GameFacade minesweeperGame = GameFacade.Instance;
-            var started = minesweeperGame.Run();
-            Assert.AreEqual(true, started);
+            string input = null;
+
+            using (var sw = new StringWriter())
+            {
+                using (var sr = new StringReader(input))
+                {
+                    Console.SetOut(sw);
+                    Console.SetIn(sr);
+
+                    // Act
+                    GameFacade minesweeperGame = GameFacade.Instance;
+                    var started = minesweeperGame.Run();
+                }
+            }
         }
     }
 }
